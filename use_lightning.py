2944d3bf0e08db2ad179d4a16815cfbdb56fb255
thonyyy <hikari.netto.355@gmail.com>
2024-05-09 14:48:18 +0000

    cleaning up code

--- use_lightning.py
+++ use_lightning.py
@@ -6,52 +6,48 @@ from torch import nn, optim
 from lightning.pytorch.callbacks import ModelCheckpoint
 import torch
 import os
+import dotenv
+if '.env' in os.listdir():
+    dotenv.load_dotenv('.env')
 
-os.environ['PJRT_DEVICE'] ='TPU'
 def collator(batch):
     keys = batch[0].keys()
     new_batch = {}
-
     for key in keys:
         if key == "labels":
             labels = torch.cat([(example[key].view(1, -1)) for example in batch], dim = 0).view(-1)
         else:
             new_batch[key] = torch.cat([(example[key].view(1, -1)) for example in batch], dim = 0)
-
     return new_batch, labels
 
-
-
-def tokenize(dataset, tokenizer, **kwargs):
-    mapping = {"negative": 0, "neutral": 1, "positive": 2}
-
+def tokenize(dataset, tokenizer, label_index):
     def batch_tokenize(batch):
         token = tokenizer(
             batch["text"],
-            max_length=128,
-            padding="max_length",
-            truncation=True,
-            return_tensors="pt",
+            max_length = 128,
+            padding = "max_length",
+            truncation = True,
+            return_tensors = "pt",
         )
 
-        token["labels"] = [mapping[label] for label in batch["label_text"]]
+        token["labels"] = [label_index[label] for label in batch["label_text"]]
         return token
 
     return dataset.map(
         batch_tokenize,
-        num_proc=16,
-        batched=True,
-        remove_columns=["text", "label_text", "source", "split"],
+        num_proc = 16,
+        batched = True,
+        remove_columns = ["text", "label_text", "source", "split"],
     )
 
-
 class Model(LightningModule):
-    def __init__(self, model_name: str = "xlm-roberta-base"):
+    def __init__(self, model_name: str = "xlm-roberta-base", label_index: dict = {}):
         super().__init__()
         self.model = AutoModelForSequenceClassification.from_pretrained(
             model_name,
-            num_labels = 3,
-            label2id = {"negative": 0, "neutral": 1, "positive": 2},
+            num_labels = len(label_index),
+            label2id = label_index,
+            id2label = {v:k for k,v in label_index.items()}
         )
         self.save_hyperparameters()
         self.loss_fn = nn.CrossEntropyLoss()
@@ -80,6 +76,7 @@ class Model(LightningModule):
         weight_decay = 0.01
         param_optimizer = list(self.named_parameters())
 
+        # Remove LayerNorm from weight decay params
         no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
         optimizer_grouped_parameters = [
             {
@@ -89,19 +86,19 @@ class Model(LightningModule):
             {"params": [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
         ]
 
-        optimizer = optim.AdamW(optimizer_grouped_parameters, lr=3e-6)
+        optimizer = optim.AdamW(optimizer_grouped_parameters, lr=float(os.getenv("LEARNING_RATE")))
         return optimizer
 
 
 class Sentence(LightningDataModule):
-    def __init__(self, model_name: str = "xlm-roberta-base", val_ratio=0.05):
+    def __init__(self, model_name: str = "xlm-roberta-base", val_ratio = 0.05):
         super().__init__()
         access_token = os.getenv("ACCESS_TOKEN")
         id_dataset = load_dataset(
-            "thonyyy/indonesian_sentiment_dataset_v1", split="train", token=access_token
+            os.getenv("ID_DATASET"), split = "train", token = access_token
         )
         en_dataset = load_dataset(
-            "thonyyy/english_sentiment_dataset_v1", split="train", token=access_token
+            os.getenv("EN_DATASET"), split = "train", token = access_token
         )
         concat_ds = concatenate_datasets([id_dataset, en_dataset]).shuffle(seed=42)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -110,29 +107,32 @@ class Sentence(LightningDataModule):
         dataset = dataset.train_test_split(test_size = val_ratio)
         self.train_dataset = dataset['train']
         self.val_dataset = dataset['test']
-        # dataset.save_to_disk()
     
     def prepare_data(self):
-        # load dataset
         pass
 
     def setup(self, stage=None):
         pass
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=512, collate_fn = collator, num_workers = 16)
+        return DataLoader(self.train_dataset, batch_size = int(os.getenv("BATCH_SIZE")), collate_fn = collator, num_workers = 16)
     
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=512, collate_fn = collator, num_workers = 16)
+        return DataLoader(self.val_dataset, batch_size = int(os.getenv("BATCH_SIZE")), collate_fn = collator, num_workers = 16)
 
 def main():
-    model = Model()
-    data = Sentence()
-    checkpoint_callback = ModelCheckpoint(monitor='val_loss')
-    trainer = Trainer(accelerator = "tpu", max_epochs=3, callbacks =  [checkpoint_callback])
+    label_index = {
+        "negative": 0,
+        "neutral": 1,
+        "positive": 2
+        }
+    model = Model(model_name = os.getenv("BASE_MODEL_NAME"), label_index = label_index)
+    data = Sentence(model_name = os.getenv("BASE_MODEL_NAME"), val_ratio = float(os.getenv("VAL_SIZE")))
+    checkpoint_callback = ModelCheckpoint(monitor = 'val_loss')
+    trainer = Trainer(accelerator = os.getenv("ACCELERATOR"), max_epochs = int(os.getenv("EPOCH")), callbacks =  [checkpoint_callback])
     trainer.fit(model, data)
-    model.model.push_to_hub("thonyyy/xlm-roberta-sentiment")
-    data.tokenizer.push_to_hub("thonyyy/xlm-roberta-sentiment")
+    model.model.push_to_hub(os.getenv("HUB_MODEL_NAME"))
+    data.tokenizer.push_to_hub(os.getenv("HUB_MODEL_NAME"))
 
 if __name__ == "__main__":
     main()
